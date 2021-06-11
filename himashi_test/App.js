8aa7929f758729d457bf01956b3a89ccfb32978f
Himashi Charuka <38151131+himashicharuka@users.noreply.github.com>
2021-06-11 17:17:51 +0530

    signIn component UI

--- himashi_test/App.js
+++ himashi_test/App.js
@@ -1,13 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet,
+   Text, 
+   View,
+  SafeAreaView,
+  TextInput,
+  StatusBar,
+  Button
+   } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <StatusBar hidden/>
+      <Text Style = {{fontSize: 14 , color : 0000}}>
+        Email
+        <TextInput 
+	Style={styles.input}
+	Placeholder='your@address.net'
+/>
+<Text Style = {{fontSize: 14 , color : 0000}}>
+Password
+<TextInput 
+	Style={styles.input}
+	Placeholder='password'
+/>
+<Button title='SignIN' onPress={() => console.log (“Button tapped ”)}/> 
+</SafeAreaView>
+);
+Input : {
+Width : “70%”,
+BackgroundColor: “ffff”,
+Padding: 15,
+margineBottom: 10,
+}
+}
   );
 }
 
